f47453b822736fb2d050b70d44b2f70b847279b1
Pingvu <hibba04@hotmail.se>
2022-09-06 11:19:13 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+   class shape 
+   {
+    protected int width;
+    protected int height;
+     public shape(int width, int height){
+        this.width = width;
+        this.height = height;
+     }
+   }
---
+++ triangle.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+    class triangle: shape
+    {
+        public triangle(): base(10,20){}
+        
+    }
