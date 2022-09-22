e4e5f6d3b1b64a0a86c87025af3bd796c29386a3
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-22 12:21:59 +0200

    aa

--- Program.cs
+++ Program.cs
@@ -4,23 +4,23 @@
 internal class program{
 
  private static void Main(){
-
+ shape shape1;
  Console.WriteLine("triangle or rectangle?");
  string shape = Console.ReadLine();
  Console.WriteLine("Write width and height: ");
  int width = int.Parse((Console.ReadLine()));
  int height = int.Parse((Console.ReadLine()));
 if (shape=="triangle"){
-    shape shape1 =new Triangle(width,height);
+    shape1 =new Triangle(width,height);
     int triArea= (shape1 as Triangle).Area();
     int triOmkr= (shape1 as Triangle).Omkr();
     Console.WriteLine(triArea);
     Console.WriteLine(triOmkr);
 }
 else if (shape== "rectangle"){
-    shape shape2 = new Rektangle (width,height);
-    int rekArea= (shape2 as Rektangle).Area();
-    int rekOmkr= (shape2 as Rektangle).Omkr();
+    shape1 = new Rektangle (width,height);
+    int rekArea= (shape1 as Rektangle).Area();
+    int rekOmkr= (shape1 as Rektangle).Omkr();
     Console.WriteLine(rekArea);
     Console.WriteLine(rekOmkr);
 }
